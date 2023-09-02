aae7b918e7537b087363bedad287ee440c512e13
tomer-shavit <tomer.shavit5@gmail.com>
2023-09-02 20:18:12 +0300

    lowered temp to 0.3 and changed logging to func.logger.info

--- Functions/ResumeTrigger/__init__.py
+++ Functions/ResumeTrigger/__init__.py
@@ -6,6 +6,7 @@ from .file_check import is_pdf
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.basicConfig()
     logging.info('Python HTTP trigger function processed a request.')
 
     # Get the resume file from the request
